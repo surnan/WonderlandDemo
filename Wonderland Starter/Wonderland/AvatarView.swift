1bb3c097ef65187d6f57d9dd6ef674526a009437
snandlall <snandlall@nyc-mac2-loaner.local>
2017-11-21 18:57:12 -0500

    completed

--- Wonderland Starter/Wonderland/AvatarView.swift
+++ Wonderland Starter/Wonderland/AvatarView.swift
@@ -1,28 +1,4 @@
-/**
- Copyright (c) 2016 Razeware LLC
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
-
 import UIKit
-
-
 
 class AvatarView: UIView {
     
@@ -111,6 +87,19 @@ class AvatarView: UIView {
         setupConstraints()
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if bounds.height < socialMediaView.bounds.height {
+            socialMediaView.alpha = 0
+        } else {
+            socialMediaView.alpha = 1
+        }
+        if imageView.bounds.height < 30 {
+            imageView.alpha = 0
+        } else {
+            imageView.alpha = 1
+        }
+    }
     
     override func updateConstraints() {
         super.updateConstraints()
@@ -126,9 +115,6 @@ class AvatarView: UIView {
     }
     
     func setup() {
-        imageView.backgroundColor = UIColor.magenta
-        titleLabel.backgroundColor = UIColor.orange
-        
         imageView.contentMode = .scaleAspectFit
         titleLabel.font = UIFont(name: "AvenirNext-Bold", size: 28.0)
         titleLabel.textColor = UIColor.black
